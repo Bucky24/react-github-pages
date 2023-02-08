9692a92726a07370b538b97f8b556ab58dec7958
Bucky24 <rwijtman@gmail.com>
2023-02-07 20:24:49 -0800

    first change

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+
+import styles from './styles.css';
+
+
+export default function App() {
+
+
+	return (<div className={styles.appRoot}>
+		Welcome to test app
+	</div>);
+}
